b89c2814a8d12063ddbbd0f2e10df6f1b1fe5a50
Leissan Khan <leissan.khan@gmail.com>
2022-01-19 18:43:27 -0500

    completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clickkkked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+addingEventListener();
